130a404b7cdb15988e95177ae71dfbe55f43775c
Markuss <markuss.jansons@sk.lvt.lv>
2024-04-22 20:02:02 +0300

    Projekta uzsākšana

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Jansons_pica {
+}
---
+++ src/pica/Jansons_pica.java
@@ -0,0 +1,10 @@
+package pica;
+
+public class Jansons_pica {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
